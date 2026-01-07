fb7007d03baeff825ee7df0441d6ed14f44fc61d
Corey B <anon@anon.com>
2026-01-07 17:53:19 +1000

    Changed Podman volume flag from Z to z so multiple instances can access the shared volumes.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,6 +5,6 @@ podman run \
   --rm \
   --log-driver=none \
   --tmpfs /tmp:rw,size=512m,mode=1777 \
-  -v "$(pwd)/../":/documents/:Z \
+  -v "$(pwd)/../":/documents/:z \
   run_ruby_tests_image \
   sh -c 'ruby test/test_*.rb'
